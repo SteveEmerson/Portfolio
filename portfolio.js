dfab215e605fb9d5dcb9242a4d49aff28af3859e
Steve Emerson <sbemerson@outlook.com>
2020-12-17 18:25:50 -0500

    adjust keyframe animation for landing page

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,12 @@
 let namePlate = document.querySelector(".name-plate");
-let quoteDiv = document.querySelector("#quote-div");
+let quoteDiv = document.createElement("div");
+quoteDiv.id = "quote-div";
+quoteDiv.innerHTML = '<p class="quote">"Not all those who wander are lost."</p><p class="quote" id="attribution">JRR Tolkien</p>'
 let nameDiv = document.createElement("div");
 nameDiv.id = "name-div";
 nameDiv.innerHTML = '<p id="name">Steve Emerson</p> <p id="job">JavaScript and Python Development</p>';
 
-setTimeout(function(){
-  namePlate.removeChild(quoteDiv);
-  namePlate.appendChild(nameDiv);
-}, 4000);
+
 
 window.onscroll = function() {scrollFunction()};
 
@@ -18,3 +17,17 @@ function scrollFunction() {
     document.getElementById("home").style.top = "-50px";
   }
 } 
+
+
+window.onload = function(){
+
+  setTimeout(function(){
+    namePlate.appendChild(quoteDiv);
+  }, 1000);
+
+  setTimeout(function(){
+    namePlate.removeChild(quoteDiv);
+    namePlate.appendChild(nameDiv);
+  }, 6000);
+  
+}
