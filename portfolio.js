bfcd6ed93e8c30229941decc8889fe505a9e4292
Steve Emerson <sbemerson@outlook.com>
2020-12-17 18:10:12 -0500

    v1.0 done

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,20 @@
-function landingFade(){
-  console.log("PAGE LOAD");
-}
+let namePlate = document.querySelector(".name-plate");
+let quoteDiv = document.querySelector("#quote-div");
+let nameDiv = document.createElement("div");
+nameDiv.id = "name-div";
+nameDiv.innerHTML = '<p id="name">Steve Emerson</p> <p id="job">JavaScript and Python Development</p>';
+
+setTimeout(function(){
+  namePlate.removeChild(quoteDiv);
+  namePlate.appendChild(nameDiv);
+}, 4000);
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("home").style.top = "0";
+  } else {
+    document.getElementById("home").style.top = "-50px";
+  }
+} 
